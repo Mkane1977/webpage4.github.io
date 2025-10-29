84fcbfdf3162c62e92c86c303ba8359092180f5b
Mercury <89310398+Mkane1977@users.noreply.github.com>
2025-10-29 19:53:31 -0400

    Fix formatting of DB_HOST definition

--- config.sample.php
+++ config.sample.php
@@ -1,6 +1,6 @@
 <?php
 // Copy this file to config.php and fill in your credentials.
-define('DB_HOST', 'localhost');      // e.g., 127.0.0.1 or host provided by your web host
+define('DB_HOST', 'localhost');      // 127.0.0.1 or host provided by your web host
 define('DB_NAME', 'your_db_name');   // database name
 define('DB_USER', 'your_db_user');   // database username
 define('DB_PASS', 'your_db_password'); // database password
